fcccbef1585c6515fbf3453d48d38c4a3d98c7b8
xzzkq <77369095@qq.com>
2019-12-19 10:49:22 +0800

    帮助中心管理

--- Coldairarrow.Business/IBusiness/Help/IDev_HelpTypeBusiness.cs
+++ Coldairarrow.Business/IBusiness/Help/IDev_HelpTypeBusiness.cs
@@ -7,6 +7,7 @@ namespace Coldairarrow.Business.Help
     public interface IDev_HelpTypeBusiness
     {
         List<Dev_HelpType> GetDataList(Pagination pagination, string condition, string keyword);
+        List<Dev_HelpType> BuildSelectResult(string selectedValueJson, string q, string textFiled, string valueField);
         Dev_HelpType GetTheData(string id);
         AjaxResult AddData(Dev_HelpType newData);
         AjaxResult UpdateData(Dev_HelpType theData);
